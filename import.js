9295bf1bb825f80fb17012bc6af7c465dd20e439
Lucasedusi <lucasmto.16@hotmail.com>
2020-06-19 20:56:50 -0300

    Config webpack

---
+++ import.js
@@ -0,0 +1,7 @@
+import soma from './funcoes'; // Importando uma função não default
+
+import { sub } from './funcoes'; // Importando uma função default
+
+import { soma as somaAll } from './funcoes'; // Importando e renomeando uma função
+
+import * as funcoes from './funcoes'; // Importando todas as funções
